165c7bb5457c229429cbbadd86d5556f85aef747
Roland Guerra <89416284+RG1-999@users.noreply.github.com>
2022-04-02 00:13:54 -0500

    Update dh_fw.sh

--- dh_fw.sh
+++ dh_fw.sh
@@ -1,7 +1,10 @@
-# sudo su
-# password: 
-root# systemctl status dhcpd.service
-root# systemctl enable dhcpd.service
-root# systemctl start dhcpd.service
-root# sudo iptables  -L 
-root# iptables -t nat -A postrouting masquerade
+touch dh_fw.sh
+vi dh_fw.sh
+#! /bin/bash
+let #sudo su
+let #password: 
+let root# systemctl status dhcpd.service
+let root# systemctl enable dhcpd.service
+let root# systemctl start dhcpd.service
+let root# sudo iptables  -L 
+let root# iptables -t nat -A postrouting masquerade
